901c1f0fd313f74dc8e27d0d63c98d52097a2988
Anand1725 <105373772+Anand1725@users.noreply.github.com>
2022-09-06 22:33:06 +0530

    Update AuthComponent.js

--- src/AuthComponent.js
+++ src/AuthComponent.js
@@ -16,7 +16,7 @@ export default function AuthComponent() {
     // set configurations for the API call here
     const configuration = {
       method: "get",
-      url: "https://nodejs-mongodb-auth-app.herokuapp.com/auth-endpoint",
+      url: "https://freecode-backend-testing.herokuapp.com/auth-endpoint",
       headers: {
         Authorization: `Bearer ${token}`,
       },
